f8384f746cb87fc86cbf78cead9b6610a92de6fe
Jeremie <jeremieberduck@icloud.com>
2021-07-08 07:52:57 +0200

    Initialising Xcode Project

---
+++ Shared/SwiftUI_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PlaygroundApp.swift
+//  Shared
+//
+//  Created by Jeremie Berduck on 08/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
