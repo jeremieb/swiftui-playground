4c15d75d26f6c7840a3623cc917381dd5dad608c
Jeremie <jeremieberduck@icloud.com>
2021-08-18 07:07:11 +0200

    Fixing list

--- Shared/LibrariesViews/GroupBoxView.swift
+++ Shared/LibrariesViews/GroupBoxView.swift
@@ -12,27 +12,36 @@ struct GroupBoxView: View {
     var body: some View {
         ScrollView {
             LazyVGrid(columns: [.init(), .init()]) {
-                ForEach(0..<10) { _ in
+                ForEach(myGroupBoxList, id: \.self) { box in
                     GroupBox(
-                        label: Label("Heart Rate", systemImage: "heart.fill")
-                            .foregroundColor(.red)
+                        label: Label(box.title, systemImage: box.icon)
+                            .foregroundColor(box.color)
                     ) {
-                        Text("Your hear rate is 90 BPM.")
+                        Text(box.description)
+                            .font(.footnote)
+                            .padding(.top, 4)
+                            .fixedSize(horizontal: false, vertical: true)
                     }.groupBoxStyle(CardGroupBoxStyle())
                 }
             }.padding()
         }
     }
     
+    private var myGroupBoxList: [MyGroupBox] = [
+        MyGroupBox(title: "Hear Rate", icon: "heart.fill", description: "You heart rate is 90 BPM.", color: .red),
+        MyGroupBox(title: "Walking", icon: "figure.walk", description: "You have walk 3.45 km today", color: .green),
+        MyGroupBox(title: "Noise", icon: "ear.fill", description: "You've been in a noisy environment today", color: .orange)
+    ]
 }
 
 // MARK: Box List
-struct MyGroupBoxList: Hashable {
+struct MyGroupBox: Hashable {
     var title: String
     var icon: String = "dot.square.fill"
     var description: String
     var color: Color = .red
 }
+
 
 // MARK: Box Main Style
 struct CardGroupBoxStyle: GroupBoxStyle {
