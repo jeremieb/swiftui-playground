e3e6437f74d9d76a9c0c7b16a12eb7f142cd5d68
Jeremie Berduck <736246+jeremieb@users.noreply.github.com>
2022-06-28 22:29:10 +0200

    Adding segmented control for chart switching

--- SwiftUI Playground 4/SwiftUI Playground 4/Views/MyCharts.swift
+++ SwiftUI Playground 4/SwiftUI Playground 4/Views/MyCharts.swift
@@ -9,27 +9,62 @@ import SwiftUI
 import Charts
 
 struct MyCharts: View {
+    
+    private enum SelectedChart {
+        case schedule
+        case clients
+    }
+
+    @State private var selected: SelectedChart = .schedule
+    
     var body: some View {
         ScrollView {
-            GroupBox("Invoices") {
-                Chart {
-                    ForEach(invoicesData, id: \.id) { invoice in
-                        BarMark (
-                            x: .value("Something", invoice.dueDate, unit: .month),
-                            y: .value(invoice.client, invoice.amount)
-                        ).foregroundStyle(Color.pink).opacity(0.4)
-                        LineMark(
-                            x: .value("Something", invoice.dueDate, unit: .month),
-                            y: .value(invoice.client, invoice.amount)
-                        ).foregroundStyle(Color.pink).interpolationMethod(.cardinal)
-                    }
-                    
-                        
-                }
-            }.frame(minHeight: 250)
-        }
+            Picker(selection: $selected, label: EmptyView()) {
+                Text("Schedule").tag(SelectedChart.schedule)
+                Text("Clients").tag(SelectedChart.clients)
+            }.pickerStyle(.segmented)
+            
+            switch selected {
+            case .schedule:
+                scheduleChart()
+            case .clients:
+                clientsChart()
+            }
+        }.animation(.easeInOut(duration: 0.450), value: selected)
         .navigationTitle("My Invoices")
         .padding()
+    }
+    
+    // MARK: Schedule Chart
+    @ViewBuilder
+    func scheduleChart() -> some View {
+        GroupBox("Schedule") {
+            Chart {
+                ForEach(invoicesData, id: \.id) { invoice in
+                    BarMark (
+                        x: .value("Schedule", invoice.dueDate, unit: .month),
+                        y: .value("Amount", invoice.amount)
+                    ).foregroundStyle(by: .value("Schedule", invoice.dueDate, unit: .month))
+                    LineMark(
+                        x: .value("Schedule", invoice.dueDate, unit: .month),
+                        y: .value("Amount", invoice.amount)
+                    ).foregroundStyle(Color.blue).interpolationMethod(.cardinal)
+                }
+            }
+        }.frame(minHeight: 250).animation(.easeInOut, value: selected)
+    }
+    
+    // MARK: Clients Chart
+    @ViewBuilder
+    func clientsChart() -> some View {
+        GroupBox("Clients") {
+            Chart(invoicesData, id: \.client) { invoice in
+                BarMark(
+                    x: .value("Amount", invoice.amount),
+                    y: .value("Clients", invoice.client)
+                ).foregroundStyle(by: .value("Clients", invoice.client))
+            }.chartLegend(.hidden)
+        }.frame(minHeight: 350).animation(.easeInOut, value: selected)
     }
 }
 
